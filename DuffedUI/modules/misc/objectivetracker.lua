c1107630e59e6ee5fd89139f0af05c424703d109
liquidbase <liquid.base.x@gmail.com>
2020-10-23 18:57:57 +0200

    Removed doubled skinned header from Objectivetracker

--- DuffedUI/modules/misc/objectivetracker.lua
+++ DuffedUI/modules/misc/objectivetracker.lua
@@ -29,7 +29,6 @@ local lA = 'http://www.wowhead.com/achievement=%d'
 local function SkinObjectiveTracker()
 	local ObjectiveTrackerFrame = _G['ObjectiveTrackerFrame']
 	local TrackerTexture = [[Interface\TargetingFrame\UI-StatusBar]]
-	ObjectiveTrackerFrame:SetScale(1.02)
 	
 	local function SkinOjectiveTrackerHeaders()
 		local frame = ObjectiveTrackerFrame.MODULES
@@ -49,23 +48,6 @@ local function SkinObjectiveTracker()
 					text:SetShadowColor(0, 0, 0, 0.4)
 					text:SetParent(header)
 					text:SetTextColor(Color.r, Color.g, Color.b, 1)
-
-					if not modules.IsSkinned then
-						local headerPanel = _G.CreateFrame('Frame', nil, header)
-						headerPanel:SetFrameLevel(header:GetFrameLevel() - 1)
-						headerPanel:SetFrameStrata('BACKGROUND')
-						headerPanel:SetPoint('TOPLEFT', 1, 1)
-						headerPanel:SetPoint('BOTTOMRIGHT', 1, 1)
-
-						local headerBar = headerPanel:CreateTexture(nil, 'ARTWORK')
-						headerBar:SetTexture('Interface\\LFGFrame\\UI-LFG-SEPARATOR')
-						headerBar:SetTexCoord(0, 0.6640625, 0, 0.3125)
-						headerBar:SetVertexColor(D.Colors.class[D.Class][1], D.Colors.class[D.Class][2], D.Colors.class[D.Class][3], D.Colors.class[D.Class][4])
-						headerBar:SetPoint('CENTER', headerPanel, -20, -4)
-						headerBar:SetSize(232, 30)
-
-						modules.IsSkinned = true
-					end
 				end
 			end
 		end
@@ -73,7 +55,6 @@ local function SkinObjectiveTracker()
 
 	local function ColorProgressBars(self, value)
 		if not (self.Bar and value) then return end
-
 		D['StatusBarColorGradient'](self.Bar, value, 100)
 	end
 
