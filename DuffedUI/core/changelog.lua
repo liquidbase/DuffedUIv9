0ee3513346d3e277a19200a3462c211967f3de39
liquidbase <liquid.base.x@gmail.com>
2020-11-25 15:33:30 +0100

    changelog

--- DuffedUI/core/changelog.lua
+++ DuffedUI/core/changelog.lua
@@ -16,6 +16,7 @@ local ChangeLogData = {
 		--"• ",
 		"• Fixed Invite-Function on guild and friend datatext",
 		"• Fixed option for flashing buffs / debuffs with 30s or less",
+		"• Update Aurora",
 	" ",
 	"Important:",
 		"|cffC41F3BThe UI automatically performs a reset of the SavedVars and installation. AddOnSkins and ProjektAzilroka are also|r",
